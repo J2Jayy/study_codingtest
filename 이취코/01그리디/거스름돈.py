c569d48000e2b6f6250ee0c4c36a664e6e244045
J2Jayy <betatest0710@gmail.com>
2024-03-28 15:50:00 +0900

    1번문제 개선

--- 이취코/01그리디/거스름돈.py
+++ 이취코/01그리디/거스름돈.py
@@ -4,7 +4,8 @@
 손님에게 거슬러 줘야할 돈이 N으로 입력될때 , 거슬러 줘야 할 동전의 최소개수는?
 (단, 거슬러 줘야할 돈 N은 항상 10의 배수이다.)
 '''
-
+'''
+# 내 시도
 N = int(input())
 Coins = 0 #동전 개수
 while(1):
@@ -23,4 +24,12 @@ while(1):
     else: 
         print(Coins)
         break
-        
+'''
+#더 나은 방법
+N = int(input())
+count = 0
+coins = [500,100,50,10]
+for coin in coins:
+    count += N//coin
+    N %= coin
+print(count)
